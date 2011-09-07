83e97bb8698568ec43ab0be845286cb0f131e80f
philip green <pmegreen@yahoo.com>
2011-09-07 14:23:09 -0400

    Created customer class and abstracted g rooting and farewell functionality into a module

--- Greeter.rb
+++ Greeter.rb
@@ -1,4 +1,42 @@
 #!/usr/bin/env ruby
 # This scipt will greet people and stuff
 
+class GreetingAndFarewell
+def name
+@name ||= "person who will give me money for some product or service"
+end
 
+def name= new_name
+@name = new_name
+end
+
+def hello 
+puts "Hello, #{@name}!"
+end
+
+def farewell
+puts "Farewell, #{@name}!"
+end
+end
+
+class Greeter
+include GreetingAndFarewell
+def initialize new_name = nil
+@name = new_name
+end
+end
+
+class Customer
+include GreetingAndFarewell
+def name
+@name ||= "guy who will provide me with a good or service in exchange for monetary compensation"
+end
+end
+
+my_greeter = Greeter.new ARGV.first
+#name = ARGV.first
+my_greeter.hello
+my_greeter.farewell
+
+# call security
+# farewell
